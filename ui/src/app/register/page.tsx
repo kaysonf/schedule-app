51bfaa7f88400eee71ece621055fbab80e5dc17b
Kayson <kaysonf94@gmail.com>
2024-11-03 15:44:13 +0800

    2

---
+++ ui/src/app/register/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'YOUR MUM',
+}
+export default function Register() {
+    return (
+        <>
+            <div>HELLO REGISTER</div>
+        </>
+    );
+}
